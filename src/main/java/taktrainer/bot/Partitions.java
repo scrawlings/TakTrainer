740f534f1943be45fdb0e6e77042d9203a78c834
Julian Kelsey <julian@nutmeg.com>
2016-05-03 13:55:37 +0100

    generate look up table for partitions

--- src/main/java/taktrainer/bot/Partitions.java
+++ src/main/java/taktrainer/bot/Partitions.java
@@ -6,21 +6,43 @@ import java.util.stream.Collectors;
 import static java.util.Arrays.asList;
 
 public class Partitions {
-    private final int size;
-    private final int[][][][] pregenerated;
+    private final int[][][][] pregenerated; // [stackHeight][chunksInPartition][partitions][chunks]
 
-    private Partitions(int size, int[][][][] pregenerated) {
-        this.size = size;
+    private Partitions(int[][][][] pregenerated) {
         this.pregenerated = pregenerated;
     }
 
-    public static Partitions partitions(final int size) {
+    public static Partitions partitions(final int maxStackHeight) {
+        int pregeneratedPartitions[][][][] = new int[maxStackHeight][maxStackHeight][][];
 
-        for (int a = 0; a < size; a++) {
-            pregenerate(a+1);
+        for (int sh = 0; sh < maxStackHeight; sh++) {
+            final int stackHeight = sh;
+
+            pregenerate(stackHeight + 1).stream()
+                    .collect(Collectors.groupingBy(p -> p.size()))
+                    .entrySet()
+                    .forEach(e -> flatten(pregeneratedPartitions[stackHeight], e.getKey(), e.getValue()));
+
         }
 
-        return new Partitions(size, null);
+        return new Partitions(pregeneratedPartitions);
+    }
+
+    private static void flatten(final int ints[][][], final int chunksInPartition, List<List<Integer>> partitionsForChunkSize) {
+        ints[chunksInPartition - 1] = new int[partitionsForChunkSize.size()][];
+        final int bucket[][] = ints[chunksInPartition - 1];
+
+        int a = 0;
+        for (List<Integer> partsList : partitionsForChunkSize) {
+            int partition[] = new int[partsList.size()];
+            int b = 0;
+            for (Integer part : partsList) {
+                partition[b] = part;
+                b += 1;
+            }
+            bucket[a] = partition;
+            a += 1;
+        }
     }
 
     private static List<List<Integer>> pregenerate(final int size) {
@@ -47,6 +69,6 @@ public class Partitions {
     }
 
     public int[][] partitions(final int depth, final int chunks) {
-        return pregenerated[chunks][depth];
+        return pregenerated[depth - 1][chunks - 1];
     }
 }
--- src/test/java/taktrainer/bot/PartitionsTest.java
+++ src/test/java/taktrainer/bot/PartitionsTest.java
@@ -2,12 +2,26 @@ package taktrainer.bot;
 
 import org.junit.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.*;
 
 public class PartitionsTest {
     @Test
     public void testPregenerateLists() throws Exception {
-        final Partitions partitions = Partitions.partitions(3);
+        final Partitions partitions = Partitions.partitions(8);
 
+        final int[][] partitions_size5_parts2 = partitions.partitions(5, 2);
+        final int[][] expected_size5_parts2 = {{1, 4}, {2, 3}, {3, 2}, {4, 1}};
+        assertThat(partitions_size5_parts2).isEqualTo(expected_size5_parts2);
+
+
+        final int[][] partitions_size8_parts1 = partitions.partitions(8, 1);
+        final int[][] expected_size8_parts1 = {{8}};
+        assertThat(partitions_size8_parts1).isEqualTo(expected_size8_parts1);
+
+
+        final int[][] partitions_size7_parts7 = partitions.partitions(7, 7);
+        final int[][] expected_size7_parts7 = {{1, 1, 1, 1, 1, 1, 1}};
+        assertThat(partitions_size7_parts7).isEqualTo(expected_size7_parts7);
     }
 }
