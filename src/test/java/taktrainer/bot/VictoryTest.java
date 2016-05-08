a39c0b55aebd9349073d98b286742fef6fd103a5
Julian Kelsey <julian.b.kelsey@gmail.com>
2016-05-08 21:39:07 +0100

    proposing some tests

--- src/test/java/taktrainer/bot/VictoryTest.java
+++ src/test/java/taktrainer/bot/VictoryTest.java
@@ -1,5 +1,6 @@
 package taktrainer.bot;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -46,5 +47,53 @@ public class VictoryTest {
         final Board board = new Board(6);
         loadTPS(board, "[TPS \"x6/x6/1,x5/2221,x5/2,x5/1,2S,2,x3 1 24\"]");
         assertThat(flatsMajority(board)).isEqualTo(P1);
+    }
+
+    @Ignore
+    @Test
+    public void testTheTwistyPath() throws Exception {
+        final Board board = new Board(6);
+        loadTPS(board, "[TPS \"1,x5/1,x1,1,1,1,x1/1,x1,1,x1,1,x1/1,x1,1,x1,1,x1/1,1,1,x1,1,x1/x4,1,x1 1 24\"]");
+        assertThat(flatsMajority(board)).isEqualTo(P1);
+    }
+
+    @Ignore
+    @Test
+    public void testTheTwistyPathBroken() throws Exception {
+        final Board board = new Board(6);
+        loadTPS(board, "[TPS \"1,x5/1,x1,1,1,1,x1/1,x3,1,x1/1,x1,1,x1,1,x1/1,1,1,x1,1,x1/x4,1,x1 1 24\"]");
+        assertThat(flatsMajority(board)).isEqualTo(NoPlayer);
+    }
+
+    @Ignore
+    @Test
+    public void testTheStraightPathDown() throws Exception {
+        final Board board = new Board(6);
+        loadTPS(board, "[TPS \"1,x5/1,x5/1,x5/1,x5/1,x5/1,x5 1 24\"]");
+        assertThat(flatsMajority(board)).isEqualTo(P1);
+    }
+
+    @Ignore
+    @Test
+    public void testTheStraightPathDownBroken() throws Exception {
+        final Board board = new Board(6);
+        loadTPS(board, "[TPS \"1,x5/1,x5/x6/1,x5/1,x5/1,x5 1 24\"]");
+        assertThat(flatsMajority(board)).isEqualTo(NoPlayer);
+    }
+
+    @Ignore
+    @Test
+    public void testTheStraightPathAcross() throws Exception {
+        final Board board = new Board(6);
+        loadTPS(board, "[TPS \"x6/x6/1,1,1,1,1,1/x6/x6/x6 1 24\"]");
+        assertThat(flatsMajority(board)).isEqualTo(P1);
+    }
+
+    @Ignore
+    @Test
+    public void testTheStraightPathAcrossBroken() throws Exception {
+        final Board board = new Board(6);
+        loadTPS(board, "[TPS \"x6/x6/1,1,1,x1,1,1/x6/x6/x6 1 24\"]");
+        assertThat(flatsMajority(board)).isEqualTo(NoPlayer);
     }
 }
