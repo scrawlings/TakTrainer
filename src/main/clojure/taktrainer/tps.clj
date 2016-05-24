71952a3f7fcb17ffaf99e06f24de879e7eeb15eb
Julian Kelsey <julian.b.kelsey@gmail.com>
2016-05-24 15:17:03 +0100

    starting to read TPS board descriptions

--- src/main/clojure/taktrainer/tps.clj
+++ src/main/clojure/taktrainer/tps.clj
@@ -1,1 +1,19 @@
 (ns taktrainer.tps)
+
+
+(defn find-size [acc row]
+  (if (empty? row)
+    acc
+    (let [blank  (= \x (first row))
+          count  (if blank (Integer/parseInt (str (second row))) 1)
+          rest   (drop 1 (drop-while #(not (= \, %)) row))]
+      (recur (+ acc count) rest))))
+
+(defn fromTPS [tps-str]
+  (let [prefix     (take 6 tps-str)
+        remainder  (drop 6 tps-str)
+        board-size (find-size 0 (take-while #(not (= \/ %)) remainder))]
+    {}))
+
+(defn toTPS [board]
+  "")
--- src/test/clojure/taktrainer/tps_test.clj
+++ src/test/clojure/taktrainer/tps_test.clj
@@ -1,3 +1,16 @@
 (ns taktrainer.tps-test
   (:require [clojure.test :refer :all]
             [taktrainer.tps :refer :all]))
+
+
+(deftest sizes
+  (is (= (find-size 0 "x6") 6))
+  (is (= (find-size 0 "1,x3,2121/x5") 5)))
+
+'(deftest load-board
+  (is (= (toTPS (fromTPS "[TPS \"x6/x6/x6/x6/x6/x6 1 1\"]")) "[TPS \"x6/x6/x6/x6/x6/x6 1 1\"]")))
+
+
+
+
+
