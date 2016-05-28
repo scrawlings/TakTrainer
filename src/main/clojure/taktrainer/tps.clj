d8a79cd81f8964eaca9d9a79846223385c57b257
Julian Kelsey <julian.b.kelsey@gmail.com>
2016-05-28 11:42:39 +0100

    read and write TPS

--- src/main/clojure/taktrainer/tps.clj
+++ src/main/clojure/taktrainer/tps.clj
@@ -9,21 +9,61 @@
           rest   (drop 1 (drop-while #(not (= \, %)) row))]
       (recur (+ acc count) rest))))
 
+
+(defn realise-stack [stack-str]
+  (let [digits    (take-while #(Character/isDigit %) stack-str)
+        flat-top  (= (count digits) (count stack-str))
+        top       (if flat-top "F" (str (last stack-str)))
+        stack     (vec (map #(keyword (str %)) digits))
+        stack     (if (not flat-top) (assoc stack (dec (count stack)) (keyword (str (last digits) top))) stack)]
+    stack))
+
 (defn realise-row [acc row-str]
   (if (empty? row-str)
     acc
-    (let [blank        (= \x (first row-str))
-          blank-count  (if blank (Integer/parseInt (str (second row-str))) nil)]
-      (vec (concat acc (repeat blank-count []))))))
+    (let [chunks          (clojure.string/split row-str #"," 2)
+          chunk           (first chunks)
+          remaining       (second chunks)
+          blank           (= \x (first chunk))
+          pos-digit       (second chunk)
+          digit-following (and blank pos-digit (Character/isDigit pos-digit))
+          blank-count     (if digit-following (Integer/parseInt (str pos-digit)) 1)
+          stack           (if (not blank) [(realise-stack chunk)] (repeat blank-count []))]
+      (recur (vec (concat acc stack)) remaining))))
 
 (defn fromTPS [tps-str]
-  (let [prefix     (subs tps-str 0 6)
-        remainder  (subs tps-str 6)
-        board      (first (clojure.string/split remainder #" " 1))
-        row-strs   (clojure.string/split board #"/")
+  (let [remainder  (subs tps-str 6)
+        parts      (clojure.string/split remainder #" " 2)
+        row-strs   (clojure.string/split (first parts) #"/")
         size       (find-size 0 (first row-strs))
-        board      (vec (map #(realise-row [] %) row-strs))]
-    {:board board :size size}))
+        board      (vec (map #(realise-row [] %) row-strs))
+        steps      (clojure.string/split (second parts) #" " 2)
+        turn       (keyword (str (read-string (first steps))))
+        move       (read-string (second steps))]
+    {:board board :size size :turn turn :move move}))
 
-(defn toTPS [board]
-  "")
+(defn render-row [row rep]
+  (if (empty? row)
+    (clojure.string/join "," rep)
+    (let [blanks        (count (take-while empty? row))
+          blanks-rep    (if (= 1 blanks) "x" (clojure.string/join ["x" (str blanks)]))
+          after-blanks  (drop-while empty? row)
+          stack         (first row)
+          stack-rep     (clojure.string/join (map name stack))
+          after-stack   (rest row)]
+      (if (> blanks 0)
+        (recur after-blanks (conj rep blanks-rep))
+        (recur after-stack (conj rep stack-rep))))))
+
+(defn render-board [board rep]
+  (if (empty? board)
+    (clojure.string/join "/" rep)
+    (let [row         (first board)
+          row-rep     (render-row row [])
+          remaining   (rest board)]
+      (recur remaining (conj rep row-rep)))))
+
+(defn toTPS [{board :board size :size turn :turn move :move}]
+  (let [tps         "[TPS \""
+        board-str   (render-board board [])]
+    (clojure.string/join [tps board-str " " (name turn) " " (str move) "\"]"])))
--- src/test/clojure/taktrainer/tps_test.clj
+++ src/test/clojure/taktrainer/tps_test.clj
@@ -7,18 +7,39 @@
   (is (= (find-size 0 "x6") 6))
   (is (= (find-size 0 "1,x3,2121/x5") 5)))
 
-'(deftest load-board
-  (is (= (toTPS (fromTPS "[TPS \"x6/x6/x6/x6/x6/x6 1 1\"]")) "[TPS \"x6/x6/x6/x6/x6/x6 1 1\"]")))
+(deftest load-board-empty
+  (is (= (toTPS (fromTPS "[TPS \"x6/x6/x6/x6/x6/x6 1 7\"]")) "[TPS \"x6/x6/x6/x6/x6/x6 1 7\"]")))
 
 
-(deftest print-board
+
+(deftest load-board-empty
+  (is (= (toTPS {:board [[[] [:1C] []]
+                         [[] [:1 :2 :2] []]
+                         [[] [] [:2S]]]
+                 :move 33
+                 :turn :2
+                 :size 3})
+         "[TPS \"x,1C,x/x,122,x/x2,2S 2 33\"]")))
+
+(deftest print-board-small
   (is (= {:board [[[] [] []]
-                  [[] [] []]
+                  [[] [:1 :2] [:2 :1S]]
                   [[] [] []]]
+          :move 13
+          :turn :2
           :size 3}
-         (fromTPS "[TPS \"x3/x3/x3 1 1\"]"))))
+         (fromTPS "[TPS \"x3/x1,12,21S/x,x2 2 13\"]"))))
 
 
-
-
+(deftest print-board-large
+  (is (= {:board [[[] [] [] [] [] []]
+                  [[] [] [] [] [] []]
+                  [[] [] [] [] [] []]
+                  [[] [] [] [] [] []]
+                  [[] [] [] [] [] []]
+                  [[] [] [] [] [] []]]
+          :move 7
+          :turn :1
+          :size 6}
+         (fromTPS "[TPS \"x6/x6/x6/x6/x6/x6 1 7\"]"))))
 
