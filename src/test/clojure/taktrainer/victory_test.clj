b7a99e53bb8503f007a1bcd77417874994bdadf5
Julian Kelsey <julian@nutmeg.com>
2016-06-09 11:35:16 +0100

    tweeking tests

--- src/test/clojure/taktrainer/victory_test.clj
+++ src/test/clojure/taktrainer/victory_test.clj
@@ -5,13 +5,7 @@
             [taktrainer.tps :refer :all]
             [taktrainer.ptn :refer :all] ))
 
-
-(def possible-results [:1 :2 :neither])
-;; slide makes to road then the person who mad the move wins
-;; equal number of flats goes to who ever has most in hand, (logically inpossible to run out in sync)
-
-(deftest flats
-  (is (= :neither (victory (fromTPS "[TPS \"111,222,11/111,222,1/x3 2 7\"]"))))
+(deftest flats-one-player-runs-out
   (is (= :1 (victory (fromTPS "[TPS \"111,222,111/111,222,1/x3 2 7\"]"))))
   (is (= :2 (victory (fromTPS "[TPS \"x,222,111/111,222,222/2,x2 2 7\"]")))))
 
