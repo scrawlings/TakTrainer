c2929cd658d99f0f8a39f9d0f404b60b172532bc
Julian Kelsey <julian.b.kelsey@gmail.com>
2016-06-09 08:00:30 +0100

    win by flats checking

--- src/main/clojure/taktrainer/victory.clj
+++ src/main/clojure/taktrainer/victory.clj
@@ -1,5 +1,32 @@
 (ns taktrainer.victory)
 
 
-(defn victory [board]
-   :neither)
+(defn played-count-cell [cell counting acc]
+  (let [controlling (first cell)]
+    (if (some #(= controlling %) counting)
+      (inc acc)
+      acc)))
+
+(defn played-count-row [row counting acc]
+  (if (empty? row)
+    acc
+    (recur (rest row) counting (played-count-cell (first row) counting acc))))
+
+(defn played-count [board counting acc]
+  (if (empty? board)
+    acc
+    (recur (rest board) counting (played-count-row (first board) counting acc))))
+
+(defn victory [{board :board turn :turn move :move {{p1C :C p1T :T} :1 {p2C :C p2T :T} :2} :pieces}]
+  (let [remaing-1   (+ p1C p1T)
+        remaing-2   (+ p2C p2T)
+        last-player (if (= turn :1) :2 :1)]
+    (if (= 0 remaing-1 remaing-2)
+      last-player
+      (if (<= 1 remaing-1 remaing-2)
+        :neither
+        (let [played-1 (played-count board  [:1C :1] 0)
+              played-2 (played-count board  [:2C :2] 0)]
+          (if (> played-1 played-2)
+            :1
+            :2))))))
--- src/test/clojure/taktrainer/victory_test.clj
+++ src/test/clojure/taktrainer/victory_test.clj
@@ -6,9 +6,17 @@
             [taktrainer.ptn :refer :all] ))
 
 
-(def possible-results :1 :2 :neither)
+(def possible-results [:1 :2 :neither])
 ;; slide makes to road then the person who mad the move wins
 ;; equal number of flats goes to who ever has most in hand, (logically inpossible to run out in sync)
 
 (deftest flats
-  (is (= :1 (victory (fromTPS "[TPS \"111,222,111/111,222,1/x3 2 7\"]")))))
+  (is (= :neither (victory (fromTPS "[TPS \"111,222,11/111,222,1/x3 2 7\"]"))))
+  (is (= :1 (victory (fromTPS "[TPS \"111,222,111/111,222,1/x3 2 7\"]"))))
+  (is (= :2 (victory (fromTPS "[TPS \"x,222,111/111,222,222/2,x2 2 7\"]")))))
+
+(deftest flats-game-not-over
+  (is (= :neither (victory (fromTPS "[TPS \"111,222,11/111,222,1/x3 2 7\"]")))))
+
+(deftest flats-last-move-wins
+  (is (= :1 (victory (fromTPS "[TPS \"111,222,111/111,222,222/2,1,x 2 7\"]")))))
