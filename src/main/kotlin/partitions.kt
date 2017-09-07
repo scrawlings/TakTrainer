bbea1071ce3b6c4964631bb6c637706db30533c1
Julian Kelsey <Julian@ip-10-33-191-90.eu-central-1.compute.internal>
2017-09-07 10:59:02 +0200

    introducing Kotlin

---
+++ src/main/kotlin/partitions.kt
@@ -0,0 +1,3 @@
+fun max(a: Int, b: Int) = if (a > b) a else b
+
+
---
+++ src/test/kotlin/partitionsTest.kt
@@ -0,0 +1,9 @@
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+
+class partitionsTest {
+    @Test
+    fun testExample() {
+        Assertions.assertThat(max(3, 4)).isEqualTo(4)
+    }
+}
